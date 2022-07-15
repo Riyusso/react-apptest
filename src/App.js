2e1784e6866fb3ce74f6fd0b8d46c16ccb8af7a5
Stefan Simeonov <stefan.simeonov@netea.bg>
2022-07-15 18:30:24 +0300

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and redeploy
+          Learn React and redeploy test
         </a>
       </header>
     </div>
